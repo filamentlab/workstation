191a6b60cab9b114f95bda3afc6b20d3fe6d6e67
Jose Guillen <guillen.jose.v@gmail.com>
2016-07-26 13:17:58 -0400

    typo

--- setup.sh
+++ setup.sh
@@ -15,9 +15,9 @@ CLEANNAME=`echo -n $CLEANNAME | tr A-Z a-z`
 CUSTOM_HOST="${CLEANNAME}.workstation.dev"
 echo -n "what's SSH Key name on Digital Ocean? [ENTER]: "
 read KEY_NAME
-echo -n "What the Digital Ocean Token? [ENTER]: "
+echo -n "What's the Digital Ocean Token? [ENTER]: "
 read TOKEN
-read -p "Aha! your workstation name will be called ${CUSTOM_HOST}. Continue? [y/n] " CHOICE
+read -p "Aha! your workstation will be called ${CUSTOM_HOST}. Continue? [y/n] " CHOICE
 
 case "$CHOICE" in y|Y )
     ./files/vagrantfile.sh $CUSTOM_HOST "$KEY_NAME" $TOKEN > Vagrantfile;echo -e "Your Vagrantfile has been created. Now it's time to vagrant up!\n";;
